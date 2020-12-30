752ec4253af8c21804cb4d73dc5175f15c32df7e
devnandito <fhersa@gmail.com>
2020-12-30 12:01:36 -0300

    refactor processData

--- processData.py
+++ processData.py
@@ -1,14 +1,16 @@
-import os, sys, re, csv, json, pandas as pd
+import os, sys, re, csv, json, pandas as pd, sqlite3
 from datetime import datetime, timedelta
 
 def menu():
     print('Select options')
-    print('\t1 Read data a save without header to csv')
-    print('\t2 Read data a save to json')
-    print('\t3 Clean quotes and save to json')
-    print('\t4 Verifycate quotes')
-    print('\t5 Clean screen')
-    print('\t6 Exit')
+    print('\t1 Clean screen')
+    print('\t2 Exit')
+    print('\t3 Read data a save without header to csv')
+    print('\t4 Read data a save to json')
+    print('\t5 Clean quotes and save to json')
+    print('\t6 Verifycate quotes')
+    print('\t7 Create query')
+    print('\t8 Execute query')
 
 def ext(data):
     ext = data.split('.')
@@ -87,16 +89,23 @@ def writeJson(handle):
             data_tmp.append(line)
     for row in data_tmp:
         stuff = row.split(':')
-        if len(stuff) == 2 or len(stuff) == 3:
+        if len(stuff) == 2:
             print(stuff[0], stuff[1])
+        elif len(stuff) == 3:
+            print(stuff[0], stuff[1], stuff[2])
         else:
             data_json.append({
                 'ci': stuff[0],
                 'first_name': stuff[1],
                 'last_name': stuff[2],
                 'birthday': stuff[3],
+                'code1': stuff[4],
                 'sex': stuff[5],
+                'type': stuff[6],
+                'age': stuff[7],
                 'nationality': stuff[8],
+                'code2': stuff[9],
+                'code3': stuff[10].rstrip(),
             })
     return data_json
 
@@ -104,8 +113,8 @@ def saveJson(ofile):
     with open(ofile, 'w+') as outfile:
         json.dump(data_json, outfile, indent=4, ensure_ascii=False)
 
-def pathFile(file_output):
-    output = os.path.join(BASE_DIR, 'dataProcess/results/'+file_output)
+def pathFile(file_output, folder):
+    output = os.path.join(BASE_DIR, 'dataProcess/'+folder+'/'+file_output)
     return output
 
 def verifQuotes(handle):
@@ -123,68 +132,42 @@ if __name__ == '__main__':
         options = input('Enter options:')
         options = options.lower()
         if options == '1':
-            start = timeNow(datetime.now())
-            try:
-                file_input = fileInput()
-            except:
-                print('File not found')
-                continue
-            file_output = fileOutput()
-            res = ext(file_input)
-            if res == 'csv' or res == 'txt':
-                data_frame = readDataFrame(file_input, res)
-                printDataFrame(data_frame)
-                ofile = pathFile(file_output)
-                data_frame.to_csv(ofile, index=False, header=False)
-            elif res == 'xlsx':
-                data_frame = readDataFrame(file_input, res)
-                printDataFrame(data_frame)
-            elif res == 'json':
-                data_frame = readDataFrame(file_input, res)
-                printDataFrame(data_frame)
-            end = timeNow(datetime.now())
-            time_run = end - start
-            print(printMessage(start, end, time_run, file_output))
-            logs = getLog()
-            count = int(logs[0])
-            name = logs[1]
-            saveLog(str(count), name, start, end, time_run, file_input)
-            count +=1
-            saveVar(count, name)
-        
+            os.system('clear')
         elif options == '2':
-            start = timeNow(datetime.now())
-            try:
-                file_input = fileInput()
-            except:
-                print('File not found')
-                continue
-            res = ext(file_input)
-            data_frame = pd.readDataFrame(file_input, res)
-            print(data_frame)
-            cols = data_frame.columns.ravel()
-            print(cols)
-            end = timeNow(datetime.now())
-            time_run = end - start
-            print(printMessage(start, end, time_run, file_input))
-            logs = getLog()
-            count = int(logs[0])
-            name = logs[1]
-            saveLog(str(count), name, start, end, time_run, file_input)
-            count +=1
-            saveVar(count, name)
+            break
         elif options == '3':
             start = timeNow(datetime.now())
-            try:
-                file_input = fileInput()
-                handle = open(file_input)
-            except:
-                print('File not found')
-                continue
+            file_input = fileInput()
             file_output = fileOutput()
-            data_json = writeJson(handle)
-            ofile = pathFile(file_output)
-            saveJson(ofile)
+            try:
+                res = ext(file_input)
+            except:
+                print('Extension not fount')
+                continue
+            folder = 'results'
+            if res == 'csv' or res == 'txt':
+                try:
+                    data_frame = readDataFrame(file_input, res)
+                    printDataFrame(data_frame)
+                    ofile = pathFile(file_output, folder)
+                    data_frame.to_csv(ofile, index=False, header=False)
+                except:
+                    print('File not found')
+                    continue
+            elif res == 'xlsx':
+                try:
+                    data_frame = readDataFrame(file_input, res)
+                    printDataFrame(data_frame)
+                except:
+                    print('File not found')
+                    continue
+            elif res == 'json':
+                try:
+                    data_frame = readDataFrame(file_input, res)
+                    printDataFrame(data_frame)
+                except:
+                    print('File not found')
+                    continue
             end = timeNow(datetime.now())
             time_run = end - start
             print(printMessage(start, end, time_run, file_output))
@@ -195,28 +178,85 @@ if __name__ == '__main__':
             count +=1
             saveVar(count, name)
         elif options == '4':
-            start = timeNow(datetime.now())
             try:
+                start = timeNow(datetime.now())
+                file_input = fileInput()
+                res = ext(file_input)
+                data_frame = readDataFrame(file_input, res)
+                printDataFrame(data_frame)
+                end = timeNow(datetime.now())
+                time_run = end - start
+                print(printMessage(start, end, time_run, file_input))
+                logs = getLog()
+                count = int(logs[0])
+                name = logs[1]
+                saveLog(str(count), name, start, end, time_run, file_input)
+                count +=1
+                saveVar(count, name)
+            except:
+                print('File not found')
+        elif options == '5':
+            try:
+                folder = 'results'
+                start = timeNow(datetime.now())
                 file_input = fileInput()
                 handle = open(file_input)
+                file_output = fileOutput()
+                data_json = writeJson(handle)
+                ofile = pathFile(file_output, folder)
+                saveJson(ofile)
+                end = timeNow(datetime.now())
+                time_run = end - start
+                print(printMessage(start, end, time_run, file_output))
+                logs = getLog()
+                count = int(logs[0])
+                name = logs[1]
+                saveLog(str(count), name, start, end, time_run, file_input)
+                count +=1
+                saveVar(count, name)
+            except:
+                print('File not found')
+        elif options == '6':
+            try:
+                start = timeNow(datetime.now())
+                file_input = fileInput()
+                handle = open(file_input)
+                verifQuotes(handle)
+                end = timeNow(datetime.now())
+                time_run = end - start
+                print(printMessage(start, end, time_run, file_input))
+                logs = getLog()
+                count = int(logs[0])
+                name = logs[1]
+                saveLog(str(count), name, start, end, time_run, file_input)
+                count +=1
+                saveVar(count, name)
+            except:
+                print('File not found')
+        elif options == '7':
+            query_name = fileOutput()
+            folder = 'queries'
+            file_save = pathFile(query_name, folder)
+            query = input('Enter query:')
+            f_open = open(file_save, 'w+')
+            f_open.write(query)
+            f_open.close()
+        elif options == '8':
+            db_name = input('Enter db name:')
+            try:
+                query = input('Enter query file:')
+                f_open = open(query)
             except:
                 print('File not found')
                 continue
-            verifQuotes(handle)
-            end = timeNow(datetime.now())
-            time_run = end - start
-            print(printMessage(start, end, time_run, file_input))
-
-            logs = getLog()
-            count = int(logs[0])
-            name = logs[1]
-            saveLog(str(count), name, start, end, time_run, file_input)
-            count +=1
-            saveVar(count, name)
-
-        elif options == '5':
-            os.system('clear')
-        elif options == '6':
-            break
+            folder = 'db'
+            path_db = pathFile(db_name, folder)
+            conn = sqlite3.connect(path_db)
+            cur = conn.cursor()
+            for line in f_open:
+                cur.execute(line)
+            conn.commit()
+            cur.close()
+            f_open.close()
         else:
             continue
---
+++ queries/query1.sql
@@ -0,0 +1,1 @@
+CREATE TABLE person (pk INTEGER PRIMARY KEY AUTOINCREMENT, ci TEXT UNIQUE, first_name TEXT, last_name TEXT, birthday TEXT, code1 TEXT, sex TEXT, type TEXT, age TEXT, nationality TEXT, code2 TEXT, code3 TEXT);
